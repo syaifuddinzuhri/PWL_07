edceedc79de84e224150b21559dd23b5f5fc40c7
Syaifuddin Zuhri <mochammadsyaifuddinzuhri@gmail.com>
2021-03-24 03:32:21 +0700

    add view in index mahasiswa

---
+++ resources/views/mahasiswa/index.blade.php
@@ -0,0 +1,47 @@
+@extends('mahasiswa.layout')
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left mt-2">
+                <h2>JURUSAN TEKNOLOGI INFORMASI-POLITEKNIK NEGERI MALANG</h2>
+            </div>
+            <div class="float-right my-2">
+                <a class="btn btn-
+                        success" href="{{ route('mahasiswas.create') }}"> Input Mahasiswa</a>
+            </div>
+        </div>
+    </div>
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+    <table class="table table-bordered">
+        <tr>
+            <th>Nim</th>
+            <th>Nama</th>
+            <th>Kelas</th>
+            <th>Jurusan</th>
+            <th>No_Handphone</th>
+            <th width="280px">Action</th>
+        </tr>
+        @foreach ($mahasiswas as $mahasiswa)
+            <tr>
+                <td>{{ $mahasiswa->nim }}</td>
+                <td>{{ $mahasiswa->nama }}</td>
+                <td>{{ $mahasiswa->kelas }}</td>
+                <td>{{ $mahasiswa->jurusan }}</td>
+                <td>{{ $mahasiswa->no_handphone }}</td>
+                <td>
+                    <form action="{{ route('mahasiswas.destroy', $mahasiswa->nim) }}" method="POST">
+                        <a class="btn btn-info" href="{{ route('mahasiswas.show', $mahasiswa->nim) }}">Show</a>
+                        <a class="btn btn-primary" href="{{ route('mahasiswas.edit', $mahasiswa->nim) }}">Edit</a>
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+    </table>
+@endsection
