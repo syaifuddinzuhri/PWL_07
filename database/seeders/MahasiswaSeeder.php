1eefe78f6e8f3dcce043334ca6b2f0f27df3b70f
Syaifuddin Zuhri <mochammadsyaifuddinzuhri@gmail.com>
2021-03-24 04:15:09 +0700

    Add new data user in MahasiswaSeeder

--- database/seeders/MahasiswaSeeder.php
+++ database/seeders/MahasiswaSeeder.php
@@ -18,6 +18,8 @@ class MahasiswaSeeder extends Seeder
             [
                 'nim' => '1941720011',
                 'nama' => 'Abdulloh Aqil',
+                'email' => 'aqil@gmail.com',
+                'tanggal_lahir' => '2000-01-13',
                 'kelas' => 'TI-2C',
                 'jurusan' => 'Teknik Informatika',
                 'no_handphone' => '085648989999',
@@ -25,6 +27,8 @@ class MahasiswaSeeder extends Seeder
             [
                 'nim' => '1941720226',
                 'nama' => 'Auful Kirom',
+                'email' => 'auful@gmail.com',
+                'tanggal_lahir' => '2000-03-12',
                 'kelas' => 'TI-2C',
                 'jurusan' => 'Teknik Informatika',
                 'no_handphone' => '085787893232',
@@ -32,6 +36,8 @@ class MahasiswaSeeder extends Seeder
             [
                 'nim' => '1941720100',
                 'nama' => 'Naily',
+                'email' => 'naily@gmail.com',
+                'tanggal_lahir' => '2001-11-13',
                 'kelas' => 'TI-2C',
                 'jurusan' => 'Teknik Informatika',
                 'no_handphone' => '082654121236',
@@ -39,6 +45,8 @@ class MahasiswaSeeder extends Seeder
             [
                 'nim' => '2041723111',
                 'nama' => 'Tita Cahyani',
+                'email' => 'tita@gmail.com',
+                'tanggal_lahir' => '2000-12-03',
                 'kelas' => 'TI-2D',
                 'jurusan' => 'Teknik Informatika',
                 'no_handphone' => '082654123123',
@@ -46,6 +54,8 @@ class MahasiswaSeeder extends Seeder
             [
                 'nim' => '1941720064',
                 'nama' => 'Shelyca',
+                'email' => 'shelyca@gmail.com',
+                'tanggal_lahir' => '2000-06-10',
                 'kelas' => 'TI-2D',
                 'jurusan' => 'Manajemen Informatika',
                 'no_handphone' => '085888321123',
@@ -53,6 +63,8 @@ class MahasiswaSeeder extends Seeder
             [
                 'nim' => '1941720234',
                 'nama' => 'Rifkie Nurdian',
+                'email' => 'rifkie@gmail.com',
+                'tanggal_lahir' => '2000-05-23',
                 'kelas' => 'TI-2C',
                 'jurusan' => 'Manajemen Informatika',
                 'no_handphone' => '085888123432',
