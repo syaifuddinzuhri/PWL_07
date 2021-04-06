57f848e9d5d6df19f87b7df19ed21ce04f885e23
Syaifuddin Zuhri <mochammadsyaifuddinzuhri@gmail.com>
2021-04-06 16:40:22 +0700

    Update view search

--- resources/views/mahasiswa/search.blade.php
+++ resources/views/mahasiswa/search.blade.php
@@ -1,16 +1,8 @@
 @extends('mahasiswa.layout')
 @section('content')
-    <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left mt-2">
-                <h2>JURUSAN TEKNOLOGI INFORMASI-POLITEKNIK NEGERI MALANG</h2>
-            </div>
-        </div>
-    </div>
-
     <div class="row my-3">
         <div class="col">
-            <a href="{{ route('mahasiswas.index') }}" class="btn btn-secondary">Kembali</a>
+            <a href="{{ route('mahasiswa.index') }}" class="btn btn-secondary">Kembali</a>
         </div>
     </div>
 
@@ -31,9 +23,9 @@
                 <td>{{ $mahasiswa->jurusan }}</td>
                 <td>{{ $mahasiswa->no_handphone }}</td>
                 <td>
-                    <form action="{{ route('mahasiswas.destroy', $mahasiswa->nim) }}" method="POST">
-                        <a class="btn btn-info" href="{{ route('mahasiswas.show', $mahasiswa->nim) }}">Show</a>
-                        <a class="btn btn-primary" href="{{ route('mahasiswas.edit', $mahasiswa->nim) }}">Edit</a>
+                    <form action="{{ route('mahasiswa.destroy', $mahasiswa->nim) }}" method="POST">
+                        <a class="btn btn-info" href="{{ route('mahasiswa.show', $mahasiswa->nim) }}">Show</a>
+                        <a class="btn btn-primary" href="{{ route('mahasiswa.edit', $mahasiswa->nim) }}">Edit</a>
                         @csrf
                         @method('DELETE')
                         <button type="submit" class="btn btn-danger">Delete</button>
