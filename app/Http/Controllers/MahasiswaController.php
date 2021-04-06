659a67e1a77313e4507b6a9a758edd34075b1859
Syaifuddin Zuhri <mochammadsyaifuddinzuhri@gmail.com>
2021-04-06 13:30:47 +0700

    Add data kelas in create method of MahasiswaController

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Kelas;
 use App\Models\Mahasiswa;
 use Illuminate\Http\Request;
 
@@ -27,7 +28,8 @@ class MahasiswaController extends Controller
      */
     public function create()
     {
-        return view('mahasiswas.create');
+        $kelas = Kelas::all();
+        return view('mahasiswas.create', compact('kelas'));
     }
 
     /**
