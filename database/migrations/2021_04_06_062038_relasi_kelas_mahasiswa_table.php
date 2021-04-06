1f4a624c8f3e50198aa2e2a800b664e370727553
Syaifuddin Zuhri <mochammadsyaifuddinzuhri@gmail.com>
2021-04-06 13:23:10 +0700

    Edit schema relasi_kelas_mahasiswa_table migration

--- database/migrations/2021_04_06_062038_relasi_kelas_mahasiswa_table.php
+++ database/migrations/2021_04_06_062038_relasi_kelas_mahasiswa_table.php
@@ -13,7 +13,11 @@ class RelasiKelasMahasiswaTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::table('mahasiswa', function (Blueprint $table) {
+            $table->dropColumn('kelas');
+            $table->unsignedBigInteger('kelas_id')->nullable();
+            $table->foreign('kelas_id')->references('id')->on('kelas');
+        });
     }
 
     /**
@@ -23,6 +27,9 @@ class RelasiKelasMahasiswaTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('mahasiswa', function (Blueprint $table) {
+            $table->string('kelas');
+            $table->dropForeign('kelasi_id');
+        });
     }
 }
