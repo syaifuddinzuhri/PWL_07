c46238191f3c3f6662b74e872a9ea74cb94b7ff0
Syaifuddin Zuhri <mochammadsyaifuddinzuhri@gmail.com>
2021-04-06 15:19:27 +0700

    Add fillable in MataKuliah Model

--- app/Models/MataKuliah.php
+++ app/Models/MataKuliah.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class MataKuliah extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nama_matkul',
+        'sks',
+        'jam',
+        'semester'
+    ];
 }
