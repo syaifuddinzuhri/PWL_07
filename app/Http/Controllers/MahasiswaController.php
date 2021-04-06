cae2421f5149debb55ffba87f8a767f35949ffe1
Syaifuddin Zuhri <mochammadsyaifuddinzuhri@gmail.com>
2021-04-06 13:29:15 +0700

    Update query in index controller Mahasiswa to adding relation with kelas

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -16,7 +16,7 @@ class MahasiswaController extends Controller
     {
         //fungsi eloquent menampilkan data menggunakan pagination
         // $mahasiswas = Mahasiswa::all(); // Mengambil semua isi tabel
-        $mahasiswas = Mahasiswa::orderBy('nim', 'desc')->paginate(5);
+        $mahasiswas = Mahasiswa::with('kelas')->orderBy('nim', 'desc')->paginate(5);
         return view('mahasiswas.index', compact('mahasiswas'))->with('i', (request()->input('page', 1) - 1) * 5);
     }
 
